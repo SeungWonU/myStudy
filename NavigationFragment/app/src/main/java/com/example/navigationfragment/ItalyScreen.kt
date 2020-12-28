ae7b3dcdf46bc7242a94c54c0d1331d745266568
SeungWon <ysw8160@naver.com>
2020-12-28 15:46:23 +0900

    craete: navigation Fragment

---
+++ NavigationFragment/app/src/main/java/com/example/navigationfragment/ItalyScreen.kt
@@ -0,0 +1,27 @@
+package com.example.navigationfragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.Navigation
+
+
+class ItalyScreen : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_italy_screen, container, false)
+
+        view.findViewById<Button>(R.id.to_london_from_italy).setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.action_italy_screen_to_london_screen)
+        }
+        view.findViewById<Button>(R.id.to_paris_from_italy).setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.action_italy_screen_to_paris_screen)
+        }
+
+        return view
+    }
+}
---
+++ NavigationFragment/app/src/main/java/com/example/navigationfragment/ParisScreen.kt
@@ -0,0 +1,26 @@
+package com.example.navigationfragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.Navigation
+
+class ParisScreen : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_paris_screen, container, false)
+
+        view.findViewById<Button>(R.id.to_italy_from_paris).setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.action_paris_screen_to_italy_screen)
+        }
+        view.findViewById<Button>(R.id.to_london_from_paris).setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.action_paris_screen_to_london_screen)
+        }
+
+        return view
+    }
+}
